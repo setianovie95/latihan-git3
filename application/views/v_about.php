4a2f47a271807515f23aaa19eaf9c4042a54cebf
“setianovie95” <adibest25@gmail.com>
2022-11-02 10:54:46 +0700

    Pert 4: Menambahkan halaman about dan menambahkan fungsi di controller web.php

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Setia</li>
+        <li>Nama Belakang : Ae</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Mampang Prapatan</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Surabaya</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Renang</li>
+        <li>Sepeda</li>
+    </ul>
+</section>
