8bd635dfe726d5bdb8625e9620273db1596f5fbd
“setianovie95” <adibest25@gmail.com>
2022-11-02 09:27:10 +0700

    Pert 2: Membuat file contoh1.php di controller

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Setia Novie
+            Saya tingga di daerah Mampang
+            olah raga yang saya sukai adalah
+            Berenang";
+    }
+}
