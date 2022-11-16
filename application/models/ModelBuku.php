af16120d38abd74b62d02258103e770d96fd60e2
“setianovie95” <adibest25@gmail.com>
2022-11-16 15:46:12 +0700

    Pert 12: Menambah file model buku di model

--- application/models/ModelBuku.php
+++ application/models/ModelBuku.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class ModelBuku extends CI_Model
 {
-    //manajemen buku
+    //manajemen buku.
     public function getBuku()
     {
         return $this->db->get('buku');
